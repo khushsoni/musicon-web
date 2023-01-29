78388e53a0cc68c275dcb092db8fad5b6f8b8ae8
Khush Soni <khushsoni555@gmail.com>
2023-01-29 13:21:39 +0530

    changes somethings

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -58,8 +58,8 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Come dive into the world of music</span>
                 <h1>{`Musicon`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Concerts", "Merchandise", "meets and greets" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                  <p>Discover new talent and intimate gigs on our platform - where small venues meet big voices.</p>
+                  <button onClick={() => console.log('#connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
     
               </div>}
             </TrackVisibility>
